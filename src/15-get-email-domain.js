3ec0896142ec1eb147a91e3e4c650031aa4c0b5a
vIv23 <vivdin3@gmail.com>
2021-04-12 01:14:45 +0300

    feat: added solution for 15-get-email-domain

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,7 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  return String(email.split('@').splice(-1));
 }
-
 module.exports = getEmailDomain;
