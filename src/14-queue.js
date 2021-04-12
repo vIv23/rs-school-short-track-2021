9030a23ed01dc12648c0a8843b1d0fddb6946384
vIv23 <vivdin3@gmail.com>
2021-04-12 22:59:08 +0300

    feat: add solution 14-querue and update 14 tests

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -10,19 +10,37 @@
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  *
  */
-
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length;
+  }
+
+  enqueue(element) {
+    const list = new ListNode(element);
+    if (this.head) {
+      this.tail.next = list;
+      this.tail = list;
+    } else {
+      this.head = list;
+      this.tail = list;
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    let del = null;
+    if (this.head !== null) {
+      del = this.head.value;
+      this.head = this.head.next;
+      this.length--;
+    }
+    return del;
   }
 }
-
 module.exports = Queue;
